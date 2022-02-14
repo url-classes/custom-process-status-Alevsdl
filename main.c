6538f9e35e976d21e14476e6ff0d2b7a8f54a0e3
Alevsdl <alejandravsdl@gmail.com>
2022-02-13 22:56:02 -0600

    tarea procesos

--- main.c
+++ main.c
@@ -1,23 +1,40 @@
+//alejandra santos
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <time.h>
 
 int main()
 {
+    // printf("Bienvenido al programa\n");
+    printf("Process Type       PID       PPID      TIME\n");
     int fork_result;
+    fork_result = fork();
+    fork_result = fork();
     fork_result = fork();
     if (fork_result >= 1)
     {
-        printf("Soy el proceso padre\n");
+        time_t t;
+        struct tm *st;
+        char *ch;
+        time(&t);
+        ch = ctime(&t) ;
+        printf("System Process      %d      %d     %s\n", getpid(), getppid(), ch);
     }
     else if (fork_result == 0)
     {
-        printf("Soy el proceso hijo\n");
+        time_t t;
+        struct tm *st;
+        char *ch;
+        time(&t);
+        ch = ctime(&t) ;
+        printf("System Process      %d      %d     %s\n", getpid(), getppid(), ch);
     }
     else
     {
-        printf("Error al crear el proceso\n");
+        printf("error");
     }
+
     return 0;
 }
